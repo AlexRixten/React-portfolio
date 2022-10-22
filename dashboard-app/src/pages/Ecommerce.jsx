44b5455bb76de8ecbae493a3bd0da7685211ce43
AlexRixten <rixten.09@gmail.com>
2022-10-22 16:32:24 +0300

    ecommerce

--- dashboard-app/src/pages/Ecommerce.jsx
+++ dashboard-app/src/pages/Ecommerce.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { IoIosMore } from "react-icons/io";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+
+import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import product9 from "../data/product9.jpg";
 
 export const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
-  )
-}
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gary-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center ">
+          <div className="flex justify-between items-center">
+            <div>
+              <p>Earning</p>
+              <p>$66,666.66</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
