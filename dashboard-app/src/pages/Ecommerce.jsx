2a9b108188840425c7934db38d40a2a9a298affd
AlexRixten <rixten.09@gmail.com>
2022-10-26 23:07:04 +0300

    is done

--- dashboard-app/src/pages/Ecommerce.jsx
+++ dashboard-app/src/pages/Ecommerce.jsx
@@ -1,37 +1,21 @@
-import React from "react";
-import { BsCurrencyDollar } from "react-icons/bs";
-import { GoPrimitiveDot } from "react-icons/go";
-import { IoIosMore } from "react-icons/io";
-import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
-import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
-import {
-  earningData,
-  medicalproBranding,
-  recentTransactions,
-  weeklyStats,
-  dropdownData,
-  SparklineAreaData,
-  ecomPieChartData,
-} from "../data/dummy";
-import { useStateContext } from "../contexts/ContextProvider";
-import product9 from "../data/product9.jpg";
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
 
 const DropDown = ({ currentMode }) => (
   <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
-    <DropDownListComponent
-      id="time"
-      fields={{ text: "Time", value: "Id" }}
-      style={{ border: "none", color: currentMode === "Dark" && "white" }}
-      value="1"
-      dataSource={dropdownData}
-      popupHeight="220px"
-      popupWidth="120px"
-    />
+    <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
   </div>
 );
 
-export const Ecommerce = () => {
+const Ecommerce = () => {
   const { currentColor, currentMode } = useStateContext();
 
   return (
@@ -52,15 +36,17 @@ export const Ecommerce = () => {
             </button>
           </div>
           <div className="mt-6">
-            <Button color="white" bgColor={currentColor} text="Download" borderRadius="10px" />
+            <Button
+              color="white"
+              bgColor={currentColor}
+              text="Download"
+              borderRadius="10px"
+            />
           </div>
         </div>
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => (
-            <div
-              key={item.title}
-              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
-            >
+            <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
@@ -70,7 +56,9 @@ export const Ecommerce = () => {
               </button>
               <p className="mt-3">
                 <span className="text-lg font-semibold">{item.amount}</span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
               </p>
               <p className="text-sm text-gray-400  mt-1">{item.title}</p>
             </div>
@@ -115,18 +103,15 @@ export const Ecommerce = () => {
               </div>
 
               <div className="mt-5">
-                <SparkLine
-                  currentColor={currentColor}
-                  id="line-sparkLine"
-                  type="Line"
-                  height="80px"
-                  width="250px"
-                  data={SparklineAreaData}
-                  color={currentColor}
-                />
+                <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} />
               </div>
               <div className="mt-10">
-                <Button color="white" bgColor={currentColor} text="Download Report" borderRadius="10px" />
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="Download Report"
+                  borderRadius="10px"
+                />
               </div>
             </div>
             <div>
@@ -135,7 +120,10 @@ export const Ecommerce = () => {
           </div>
         </div>
         <div>
-          <div className=" rounded-2xl md:w-400 p-4 m-3" style={{ backgroundColor: currentColor }}>
+          <div
+            className=" rounded-2xl md:w-400 p-4 m-3"
+            style={{ backgroundColor: currentColor }}
+          >
             <div className="flex justify-between items-center ">
               <p className="font-semibold text-white text-2xl">Earnings</p>
 
@@ -146,15 +134,7 @@ export const Ecommerce = () => {
             </div>
 
             <div className="mt-4">
-              <SparkLine
-                currentColor={currentColor}
-                id="column-sparkLine"
-                height="100px"
-                type="Column"
-                data={SparklineAreaData}
-                width="320"
-                color="rgb(242, 252, 253)"
-              />
+              <SparkLine currentColor={currentColor} id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
             </div>
           </div>
 
@@ -202,7 +182,12 @@ export const Ecommerce = () => {
           </div>
           <div className="flex justify-between items-center mt-5 border-t-1 border-color">
             <div className="mt-3">
-              <Button color="white" bgColor={currentColor} text="Add" borderRadius="10px" />
+              <Button
+                color="white"
+                bgColor={currentColor}
+                text="Add"
+                borderRadius="10px"
+              />
             </div>
 
             <p className="text-gray-400 text-sm">36 Recent Transactions</p>
@@ -249,17 +234,10 @@ export const Ecommerce = () => {
               </div>
             ))}
             <div className="mt-4">
-              <SparkLine
-                currentColor={currentColor}
-                id="area-sparkLine"
-                height="160px"
-                type="Area"
-                data={SparklineAreaData}
-                width="320"
-                color="rgb(242, 252, 253)"
-              />
+              <SparkLine currentColor={currentColor} id="area-sparkLine" height="160px" type="Area" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
             </div>
           </div>
+
         </div>
         <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
           <div className="flex justify-between">
@@ -305,7 +283,12 @@ export const Ecommerce = () => {
           </div>
           <div className="flex justify-between items-center mt-5 border-t-1 border-color">
             <div className="mt-3">
-              <Button color="white" bgColor={currentColor} text="Add" borderRadius="10px" />
+              <Button
+                color="white"
+                bgColor={currentColor}
+                text="Add"
+                borderRadius="10px"
+              />
             </div>
 
             <p className="text-gray-400 text-sm">36 Recent Transactions</p>
@@ -319,15 +302,25 @@ export const Ecommerce = () => {
             </button>
           </div>
           <div className="mt-10">
-            <img className="md:w-96 h-50 " src={product9} alt="" />
+            <img
+              className="md:w-96 h-50 "
+              src={product9}
+              alt=""
+            />
             <div className="mt-8">
               <p className="font-semibold text-lg">React 18 coming soon!</p>
               <p className="text-gray-400 ">By Johnathan Doe</p>
               <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown here. There could be some great info.
+                This will be the small description for the news you have shown
+                here. There could be some great info.
               </p>
               <div className="mt-3">
-                <Button color="white" bgColor={currentColor} text="Read More" borderRadius="10px" />
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="Read More"
+                  borderRadius="10px"
+                />
               </div>
             </div>
           </div>
@@ -336,3 +329,5 @@ export const Ecommerce = () => {
     </div>
   );
 };
+
+export default Ecommerce;
