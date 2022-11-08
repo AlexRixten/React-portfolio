d763de5d0638df87ccc82d429860dc01280a1874
AlexRixten <rixten.09@gmail.com>
2022-11-08 23:49:09 +0300

    create utils data

--- shareme-app/shareme-frontend/src/container/Home.jsx
+++ shareme-app/shareme-frontend/src/container/Home.jsx
@@ -7,13 +7,42 @@ import { Sidebar, UserProfile } from "../components";
 import { Pins } from "./Pins";
 import { client } from "../client";
 import logo from "../assets/logo.png";
+import { userQuery } from "../utils/data";
 
 export const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const userInfo =
+    localStorage.getItem("user") !== "undefined" ? JSON.parse(localStorage.getItem("user")) : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => setUser(data[0]));
+  }, []);
+
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out ">
       <div className="hidden md:flex  h-screen flex-initial ">
-         <Sidebar />
+        <Sidebar />
       </div>
+      <div className="flex md:hidden flex-row ">
+        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 " />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="profileAvatar" className="w-28 " />
+        </Link>
+      </div>
+      {toggleSidebar ? (
+        <div className="fixed w-4/5 bg-white h0screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
