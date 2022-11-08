b1ad02d2af3f629e218a98751334870f65629434
AlexRixten <rixten.09@gmail.com>
2022-11-08 23:28:39 +0300

    fix

--- shareme-app/shareme-frontend/src/container/Home.jsx
+++ shareme-app/shareme-frontend/src/container/Home.jsx
@@ -5,15 +5,15 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import { Sidebar, UserProfile } from "../components";
 import { Pins } from "./Pins";
-import { client  } from "../client";
-import logo from '../assets/logo.png'
+import { client } from "../client";
+import logo from "../assets/logo.png";
 
 export const Home = () => {
   return (
-    <div className="flex bg-gray-50 md:flex-col ">
-      Home
-      <Sidebar />
-      <UserProfile />
-    </div> 
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out ">
+      <div className="hidden md:flex  h-screen flex-initial ">
+         <Sidebar />
+      </div>
+    </div>
   );
 };
